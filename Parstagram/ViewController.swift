e1b381bfa41aa14dba9c4848ce04bd4e9950461e
Amanuel Tesfamichael <amantesfazgi19@gmail.com>
2020-03-13 19:10:36 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Amanuel Tesfazgi Tesfamichael on 2020/03/13.
+//  Copyright © 2020 Amanuel Tesfazgi Tesfamichael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
